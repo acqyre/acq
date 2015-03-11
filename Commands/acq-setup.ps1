f528c13035ffdf8241b33744f4a5a58772bc1dc7
anurse <andrew@andrewnurse.net>
2015-03-11 10:43:22 -0700

    fix setup

--- Commands/acq-setup.ps1
+++ Commands/acq-setup.ps1
@@ -1,5 +1,5 @@
 $script = @"
-Import-Module $PSScriptRoot\..\Acq\Acqyre.psd1
+Import-Module $($LibraryPaths.Root)\Acq\Acqyre.psd1
 try {
     acq @args
 } finally {
@@ -9,3 +9,13 @@ try {
 $script | Out-File -Encoding Ascii -FilePath (Join-Path $LibraryPaths.Bin "acq.ps1")
 
 "@powershell -NoLogo -File %~dp0acq.ps1 %*" | Out-File -Encoding Ascii -FilePath (Join-Path $LibraryPaths.Bin "acq.cmd")
+
+# Set up environment variables
+$userPath = [Environment]::GetEnvironmentVariable("PATH", "User")
+$userPath = Add-Path "%ACQYRE_LIBRARY%\Bin" $userPath
+[Environment]::SetEnvironmentVariable("PATH", $userPath, "User")
+
+[Environment]::SetEnvironmentVariable("ACQYRE_LIBRARY", $LibraryPaths.Root, "User")
+
+$env:PATH = Add-Path "$env:ACQYRE_LIBRARY\Bin" $env:PATH
+$env:ACQYRE_LIBRARY = $LibraryPaths.Root
