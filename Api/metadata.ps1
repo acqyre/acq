1317f5dda3c383ce6569aa45db887b280b381c2f
Andrew Nurse <andrew@andrewnurse.net>
2015-04-01 10:35:12 -0700

    Update metadata.ps1

--- Api/metadata.ps1
+++ Api/metadata.ps1
@@ -1,7 +1,21 @@
 function version() {
     param(
-        [Parameter(Mandatory=$true, Position=0)][string]$Version)
+        [Parameter(Mandatory=$true, Position=0)][string]$Value)
     
-    Add-Member "Version" $Version -InputObject $_CurrentSpec -Force
+    Add-Member "Version" $Value -InputObject $_CurrentSpec -Force
+}
+
+function author() {
+    param(
+        [Parameter(Mandatory=$true, Position=0)][string]$Value)
+    
+    Add-Member "Author" $Value -InputObject $_CurrentSpec -Force
+}
+
+function packager() {
+    param(
+        [Parameter(Mandatory=$true, Position=0)][string]$Value)
+    
+    Add-Member "Packager" $Value -InputObject $_CurrentSpec -Force
 }
 
